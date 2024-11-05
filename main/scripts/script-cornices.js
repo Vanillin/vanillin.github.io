22d7cebbebb63f19643145168f9413eaa7cb76b5
Vanillin <ivan.elistrat@mail.ru>
2024-11-05 23:10:59 +0300

    Changed left navigation menu position to sticky, changed catalog

--- main/scripts/script-cornices.js
+++ main/scripts/script-cornices.js
@@ -15,26 +15,26 @@ function CreateNewElement(name, img, price, lenght, wight, heigth){
 }
 
 // CreateNewElement("K.01.01", "main/images/models-K/K 1.1 25.30.png", "655", 1000, 25, 30); //сложный
-CreateNewElement("K.01.07", "main/images/models-K/K 1.7 20.20.png", 595 -200 , 1000, 20, 20);
+//CreateNewElement("K.01.07", "main/images/models-K/K 1.7 20.20.png", 595 -200 , 1000, 20, 20); //убрать
 CreateNewElement("K.01.08", "main/images/models-K/K 1.8 25.25.png", 608 -200 , 1000, 25, 25);
 CreateNewElement("K.01.02", "main/images/models-K/K 1.2 30.30.png", 630 -200 , 1000, 30, 30);
-CreateNewElement("K.01.03", "main/images/models-K/K 1.3 30.30.png", 593 -200 , 1000, 30, 30);
+//CreateNewElement("K.01.03", "main/images/models-K/K 1.3 30.30.png", 593 -200 , 1000, 30, 30); //убрать
 CreateNewElement("K.01.09", "main/images/models-K/K 1.9 30.30.png", 645 -200 , 1000, 30, 30);
-CreateNewElement("K.01.04", "main/images/models-K/K 1.4 20.32.png", 644 -200 , 1000, 20, 32);
+//CreateNewElement("K.01.04", "main/images/models-K/K 1.4 20.32.png", 644 -200 , 1000, 20, 32); //убрать
 CreateNewElement("K.01.05", "main/images/models-K/K 1.5 35.40.png", 696 -200 , 1000, 35, 40);
 CreateNewElement("K.01.06", "main/images/models-K/K 1.6 36.40.png", 654 -200 , 1000, 36, 40);
 
 CreateNewElement("K.02.12", "main/images/models-K/K 2.12 40.41.png", 720 -150 , 1000, 40, 41);
-CreateNewElement("K.02.08", "main/images/models-K/K 2.8 43.43.png", 781 -150 , 1000, 43, 43);
+//CreateNewElement("K.02.08", "main/images/models-K/K 2.8 43.43.png", 781 -150 , 1000, 43, 43); //убрать
 CreateNewElement("K.02.13", "main/images/models-K/K 2.13 28.45.png", 707 -150 , 1000, 28, 45);
 CreateNewElement("K.02.14", "main/images/models-K/K 2.14 30.45.png", 736 -150 , 1000, 30, 45);
 CreateNewElement("K.02.11", "main/images/models-K/K 2.11 37.45.png", 784 -150 , 1000, 37, 45);
 CreateNewElement("K.02.01", "main/images/models-K/K 2.1 45.45.png", 789 -150 , 1000, 45, 45);
 CreateNewElement("K.02.07", "main/images/models-K/K 2.7 46.49.png", 817 -150 , 1000, 46, 49);
-CreateNewElement("K.02.10", "main/images/models-K/K 2.10 40.50.png", 786 -150 , 1000, 40, 50);
+CreateNewElement("K.02.10", "main/images/models-K/K 2.10 40.50.png", 786 -150 + 500, 1000, 40, 50);
 CreateNewElement("K.02.02", "main/images/models-K/K 2.2 50.50.png", 812 -150 , 1000, 50, 50);
 CreateNewElement("K.02.03", "main/images/models-K/K 2.3 55.56.png", 788 -150 , 1000, 55, 56);
-CreateNewElement("K.02.09", "main/images/models-K/K 2.9 53.58.png", 909 -150 , 1000, 53, 58);
+//CreateNewElement("K.02.09", "main/images/models-K/K 2.9 53.58.png", 909 -150 , 1000, 53, 58); //убрать
 CreateNewElement("K.02.04", "main/images/models-K/K 2.4 60.60.png", 917 -150 , 1000, 60, 60);
 
 // CreateNewElement("K.03.01", "main/images/models-K/K 3.1 60.60.png", "740", 1000, 60, 60); //копия
@@ -43,11 +43,11 @@ CreateNewElement("K.03.09", "main/images/models-K/K 2.6 70.52.png", 840 -100 , 1
 CreateNewElement("K.03.06", "main/images/models-K/K 3.6 70.70.png", 1053 -100 , 1000, 70, 70);
 CreateNewElement("K.03.05", "main/images/models-K/K 3.5 75.72.png", 1124 -100 , 1000, 75, 72);
 CreateNewElement("K.03.02", "main/images/models-K/K 3.2 80.80.png", 1010 -100 , 1000, 80, 80);
-CreateNewElement("K.03.07", "main/images/models-K/K 3.7 80.80.png", 1255 -100 , 1000, 80, 80);
+//CreateNewElement("K.03.07", "main/images/models-K/K 3.7 80.80.png", 1255 -100 , 1000, 80, 80); //убрать
 
-CreateNewElement("K.04.05", "main/images/models-K/K 4.5 81.83.png", 1034, 1000, 81, 83);
+//CreateNewElement("K.04.05", "main/images/models-K/K 4.5 81.83.png", 1034, 1000, 81, 83); //убрать
 CreateNewElement("K.04.09", "main/images/models-K/K 3.3 85.80.png", 1100, 1000, 85, 80); //3.3
-CreateNewElement("K.04.04", "main/images/models-K/K 4.4 85.84.png", 1309, 1000, 85, 84);
+//CreateNewElement("K.04.04", "main/images/models-K/K 4.4 85.84.png", 1309, 1000, 85, 84); //убрать
 CreateNewElement("K.04.01", "main/images/models-K/K 4.1 85.85.png", 1323, 1000, 85, 85);
 CreateNewElement("K.04.06", "main/images/models-K/K 4.6 98.98.png", 1573, 1000, 98, 98);
 CreateNewElement("K.04.08", "main/images/models-K/K 4.8 50.100.png", 1124, 1000, 50, 100);
@@ -56,12 +56,12 @@ CreateNewElement("K.04.03", "main/images/models-K/K 4.3 100.100.png", 1603, 1000
 
 CreateNewElement("K.05.06", "main/images/models-K/K 4.7 72.104.png", 1339, 1000, 72, 104); //4.7
 CreateNewElement("K.05.07", "main/images/models-K/K 3.4 105.80.png", 1201, 1000, 105, 80); //3.4
-CreateNewElement("K.05.05", "main/images/models-K/K 5.5 100.113.png", 1789, 1000, 100, 113);
-CreateNewElement("K.05.01", "main/images/models-K/K 5.1 100.115.png", 1809, 1000, 100, 115);
+//CreateNewElement("K.05.05", "main/images/models-K/K 5.5 100.113.png", 1789, 1000, 100, 113); //убрать
+//CreateNewElement("K.05.01", "main/images/models-K/K 5.1 100.115.png", 1809, 1000, 100, 115); //убрать
 CreateNewElement("K.05.08", "main/images/models-K/K 4.9 80.118.png", 1506, 1000, 80, 118); //4.9
 CreateNewElement("K.05.04", "main/images/models-K/K 5.4 75.120.png", 1252, 1000, 75, 120);
 CreateNewElement("K.05.02", "main/images/models-K/K 5.2 80.120.png", 1537, 1000, 80, 120);
 CreateNewElement("K.05.03", "main/images/models-K/K 5.3 115.120.png", 1981, 1000, 115, 120);
 
 CreateNewElement("K.06.02", "main/images/models-K/K 6.2 130.128.png", 1821, 1000, 130, 128);
-CreateNewElement("K.06.01", "main/images/models-K/K 6.1 198.130.png", 3189, 1000, 198, 130);
+CreateNewElement("K.06.01", "main/images/models-K/K 6.1 198.130.png", 3189 - 400, 1000, 198, 130);
--- main/scripts/script-moldings.js
+++ main/scripts/script-moldings.js
@@ -15,26 +15,26 @@ function CreateNewElement(name, img, price, lenght, wight, heigth){
 }
 
 CreateNewElement("M.01.01", "main/images/models-M/M 1.1 20.9.png", 333 -200 , 1000, 9, 20);
-CreateNewElement("M.01.02", "main/images/models-M/M 1.2 24.12.png", 419 -200 , 1000, 12, 24);
-CreateNewElement("M.01.07", "main/images/models-M/M 1.7 25.8.png", 394 -200 , 1000, 8, 25);
+CreateNewElement("M.01.02", "main/images/models-M/M 1.2 24.12.png", 419 -200 + 60 , 1000, 12, 24);
+CreateNewElement("M.01.07", "main/images/models-M/M 1.7 25.8.png", 394 -200 + 36, 1000, 8, 25);
 CreateNewElement("M.01.04", "main/images/models-M/M 1.4 25.10.png", 360 -200 , 1000, 10, 25);
-CreateNewElement("M.01.08", "main/images/models-M/M 1.8 25.10.png", 395 -200 , 1000, 10, 25);
-CreateNewElement("M.01.06", "main/images/models-M/M 1.6 25.12.png", 436 -200 , 1000, 12, 25);
-CreateNewElement("M.01.05", "main/images/models-M/M 1.5 30.10.png", 396 -200 , 1000, 10, 30);
-CreateNewElement("M.01.03", "main/images/models-M/M 1.3 30.13.png", 482 -200 , 1000, 13, 30);
-CreateNewElement("M.01.10", "main/images/models-M/M 1.10 35.12.png", 513 -200 , 1000, 12, 35);
-CreateNewElement("M.01.09", "main/images/models-M/M 1.9 38.12.png", 480 -200 , 1000, 12, 38);
+CreateNewElement("M.01.08", "main/images/models-M/M 1.8 25.10.png", 395 -200 + 55, 1000, 10, 25);
+//CreateNewElement("M.01.06", "main/images/models-M/M 1.6 25.12.png", 436 -200 , 1000, 12, 25); //убрать
+CreateNewElement("M.01.05", "main/images/models-M/M 1.5 30.10.png", 396 -200 , 1000, 16, 31);
+CreateNewElement("M.01.03", "main/images/models-M/M 1.3 30.13.png", 482 -200 , 1000, 13, 29);
+CreateNewElement("M.01.10", "main/images/models-M/M 1.10 35.12.png", 513 -200 , 1000, 14, 35);
+CreateNewElement("M.01.09", "main/images/models-M/M 1.9 38.12.png", 480 -200 , 1000, 12, 36);
 
-CreateNewElement("M.02.01", "main/images/models-M/M 2.1 41.16.png", 621 -150 , 1000, 16, 41);
-CreateNewElement("M.02.08", "main/images/models-M/M 2.8 42.17.png", 667 -150 , 1000, 17, 42);
+CreateNewElement("M.02.01", "main/images/models-M/M 2.1 41.16.png", 621 -150 + 14, 1000, 16, 41);
+//CreateNewElement("M.02.08", "main/images/models-M/M 2.8 42.17.png", 667 -150 , 1000, 17, 42); //убрать повтор
 CreateNewElement("M.02.02", "main/images/models-M/M 2.2 45.15.png", 631 -150 , 1000, 15, 45);
 CreateNewElement("M.02.03", "main/images/models-M/M 2.3 46.12.png", 540 -150 , 1000, 12, 46);
-CreateNewElement("M.02.06", "main/images/models-M/M 2.6 50.22.png", 809 -150 , 1000, 22, 50);
-CreateNewElement("M.02.04", "main/images/models-M/M 2.4 55.17.png", 756 -150 , 1000, 17, 55);
-CreateNewElement("M.02.05", "main/images/models-M/M 2.5 60.25.png", 1070 -150 , 1000, 25, 60);
-CreateNewElement("M.02.07", "main/images/models-M/M 2.7 60.25.png", 1075 -150 , 1000, 25, 60);
+CreateNewElement("M.02.06", "main/images/models-M/M 2.6 50.22.png", 809 -150 +100 , 1000, 21, 50);
+//CreateNewElement("M.02.04", "main/images/models-M/M 2.4 55.17.png", 756 -150 , 1000, 17, 55); убрать
+//CreateNewElement("M.02.05", "main/images/models-M/M 2.5 60.25.png", 1070 -150 , 1000, 25, 60); убрать
+CreateNewElement("M.02.07", "main/images/models-M/M 2.7 60.25.png", 1075 -150 -200, 1000, 23, 59);
 
-CreateNewElement("M.03.02", "main/images/models-M/M 3.2 70.17.png", 883 -100 , 1000, 17, 70);
-CreateNewElement("M.03.01", "main/images/models-M/M 3.1 80.30.png", 1473 -100 , 1000, 30, 80);
+//CreateNewElement("M.03.02", "main/images/models-M/M 3.2 70.17.png", 883 -100 , 1000, 17, 70); убрать
+CreateNewElement("M.03.01", "main/images/models-M/M 3.1 80.30.png", 1473 -100 -400, 1000, 30, 80);
 
-CreateNewElement("M.04.01", "main/images/models-M/M 4.1 95.25.png", 1460, 1000, 25, 95);
+CreateNewElement("M.04.01", "main/images/models-M/M 4.1 95.25.png", 1460, 1000 -400, 25, 95);
